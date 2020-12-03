897195465cee5e54183bbb3bec485348132b0f6d
dcbryant <cong.ding@100credit.com>
2020-12-03 20:09:00 +0800

    feat 11

---
+++ leet/11.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const detectCycle = function(head) {
+    let fast = head;
+    let slow = head;
+    while (fast && fast.next) {
+      fast = fast.next.next;
+      slow = slow.next;
+
+      if (slow === fast) {
+        fast = head;
+        while (fast !== slow) {
+          fast = fast.next
+          slow = slow.next
+        }
+        return fast
+      } 
+    }
+
+
+    return null;
+};
